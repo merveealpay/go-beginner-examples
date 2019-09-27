d8e5f2e9df71daea2c71b43e002cceed7a897e96
merve <36377574+merveealpay@users.noreply.github.com>
2019-09-27 22:04:36 +0300

    add first examples

---
+++ types-variables.go
@@ -0,0 +1,58 @@
+package main
+
+func main() {
+	/*
+
+		fmt.Println("1 + 1 =", 1+1)
+
+		var name string = "golang" // değişken adı küçük harfle başlarsa dışarıdan erişilemez olur
+		fmt.Println(name)
+		var Version string = "1.2" // değişken adı büyük harfle başlarsa public olur
+		fmt.Println(Version)
+
+	*/
+
+	/*
+
+		var message string
+		message = "Merhaba Merve"
+		//var message string = "Merhaba"
+		//var message = "Merhaba Merve"
+		fmt.Println(message)
+
+	*/
+
+	/*
+
+		var a, b, c int = 3, 4, 5
+
+		fmt.Println(a)
+		fmt.Println(b)
+		fmt.Println(c)
+
+	*/
+
+	//tanımladıgımız degiskenleri kullanmazsak hata alıyoruz, yorum satırı yapabiliriz eğer kullanmayacaksak.
+
+	/*
+
+		var k, o string = "abc" , "xyz"
+		var  p = 42
+
+		u :=55 //herhangi bir tip belirtmeden bu şekilde tanımlama da yapabiliriz.
+		v, n := "abc",true
+		message := "Merhaba Go"
+		k, l, m := 4, "You", false
+
+	*/
+
+	/*
+
+		a := "merve" // fakat function içindeyken bunu bu şekilde kullanabiliriz, global alanda bunu bu şekilde tanımlayamayız izin vermiyor go.
+		c := 'M' // char tipinde , karakter. Bunun çıktısı ASCII kodunu verecektir
+
+		fmt.Println(a,b,c)
+
+	*/
+
+}
