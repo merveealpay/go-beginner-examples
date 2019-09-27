95f2d1e2a34b463cf6adcad2006a075b40304e68
merve <36377574+merveealpay@users.noreply.github.com>
2019-09-27 22:36:32 +0300

    add multiple variables exercises

---
+++ multiple.go
@@ -0,0 +1,20 @@
+//Multiple Variables/Constants
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		a = 5
+		b = 8
+	)
+
+	const (
+		pi = 3.14
+		x  = "world"
+	)
+
+	fmt.Println(a, b, pi, x)
+}
